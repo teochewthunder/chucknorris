f01477ddeeb5e83b3ae9a5a8b3580d9d6bd99c1e
Roger Tan <teochewthunder@gmail.com>
2026-03-23 14:48:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>In Memory of Chuck Norris</title>
+
+		<style>
+			body
+			{
+				background: url(chucknorris.jpg) left top no-repeat;
+				background-size: cover;
+				font-size: 60px;
+				font-family: georgia;
+				color: rgb(255, 100, 0);
+				text-shadow: -2px -2px 2px rgb(255, 255, 255), 2px -2px 2px rgb(255, 255, 255), -2px  2px 2px rgb(255, 255, 255), 2px  2px 2px rgb(255, 255, 255);
+			}
+
+			.number
+			{
+				font-weight: bold;
+				width: 10em;
+				float: left;
+			}
+
+			.fact
+			{
+				width: 20em;
+				float: left;
+			}
+		</style>
+
+		<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
+
+		<script>
+			$(document).ready(function() {
+
+			});
+		</script>
+	</head>
+
+	<body>
+		<div class="number">Fact #112200:</div>
+		<br />
+		<div class="fact">Chuck Norris can divide by zero.</div>
+	</body>
+</html>
